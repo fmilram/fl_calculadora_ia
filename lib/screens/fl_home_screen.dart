5b9002e7217644ec4531f05c6e12b3e880c3aa87
fmilram <franmilanramos@gmail.com>
2026-03-22 17:31:23 +0100

    Logout

--- lib/screens/fl_home_screen.dart
+++ lib/screens/fl_home_screen.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  Future<void> _cerrarSesion(BuildContext context) async {
+    bool confirmar =
+        await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Cerrar sesión'),
+            content: const Text('¿Seguro que quieres cerrar sesión?'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context, false),
+                child: const Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, true),
+                child: const Text('Sí'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+
+    if (confirmar) {
+      await FirebaseAuth.instance.signOut();
+
+      if (!context.mounted) return;
+
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       body: const Center(child: Text('Login correcto')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _cerrarSesion(context),
+        child: const Icon(Icons.logout),
+      ),
     );
   }
 }
